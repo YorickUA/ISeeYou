4204a6e4b51098b13e3f02e68caedb3082f15182
Pavlo.Livchak <pavlo.livchak@eliftech.com>
2017-11-29 11:30:41 +0200

    Minor refactoring, installation steps on raspberry Pi added into README.md

--- README.md
+++ README.md
@@ -1,20 +1,23 @@
 # ISeeYou
-## AWS Rekognition experiment.
+## AWS Rekognition experiment
 ### General operation principle
-This program is used for AWS Rekognition service testing. First of all, user need to create the AWS Rekognition collection from the images, previously uploaded to the AWS S3 bucket. After this, user can perform the comparison between images stored in the collection and any image he wants. 
+This program is used for AWS Rekognition service testing. First of all, user need to create the AWS Rekognition collection from the images, previously uploaded to the AWS S3 bucket. After this, user can perform the comparison between images stored in the collection and any image he wants.
 
 How to start:
 1. git clone
-2. Set the AWS keys by running aws configure
-3. Run 
+2. Set the AWS keys by running
+```
+aws configure
+```
+3. Run
 ```
 npm install
 ```
-4. Run 
+4. Run
 ```
 ./node_modules/.bin/webpack
 ```
-5. Run 
+5. Run
 ```
 node index
 ```
@@ -24,3 +27,26 @@ node index
 How to use:
 1. Add persons and their images into the database.
 2. Push the scan button when camera facing person you want to check against database images.
+
+## Installation on raspberry pi
+Steps:
+1. Install nodejs (tested on v6.11.4 and v9.2.0)
+2. Install and compile opencv https://docs.opencv.org/2.4/doc/tutorials/introduction/linux_install/linux_install.html#linux-installation. Consider the increasing of swap size to speed up compilation. (Might take a lot of time)
+3. In /etc/ld.so.conf.d/opencv.conf add the path to lib folder in compiled opencv folder.
+4. Install aws-cli and set up aws keys by running
+```
+aws configure
+```
+4. In raspberry folder of this repository run
+```
+npm install
+```
+Migth be a permition issue with opencv, if this happens run
+```
+npm install --unsafe-perm
+```
+5. Run
+```
+node index
+```
+in raspberry folder to start monitoring.
--- raspberry/index.js
+++ raspberry/index.js
@@ -1,5 +1,11 @@
 var cv = require('opencv'),
-    AWS = require('aws-sdk');
+    AWS = require('aws-sdk'),
+    camWidth,
+    camHeight,
+    camFps,
+    camInterval,
+    camera,
+    counter;
 
 AWS.config.update({region:'eu-west-1'});
 AWS.config.setPromisesDependency(null);
@@ -8,33 +14,27 @@ dynamodb = new AWS.DynamoDB();
 rekognition = new AWS.Rekognition();
 
 // camera properties
-var camWidth = 320;
-var camHeight = 240;
-var camFps = 10;
-var camInterval = 10000 / camFps;
-var counter = 0;
-
-// face detection properties
-var rectColor = [0, 255, 0];
-var rectThickness = 2;
+camWidth = 320;
+camHeight = 240;
+camFps = 10;
+camInterval = 10000 / camFps;
+counter = 0;
 
 // initialize camera
-var camera = new cv.VideoCapture(0);
+camera = new cv.VideoCapture(0);
 camera.setWidth(camWidth);
 camera.setHeight(camHeight);
 
 
 setInterval(function() {
     camera.read(function(err, im) {
-
         console.log("scan")
         if (err) throw err;
         im.detectObject('./node_modules/opencv/data/haarcascade_frontalface_alt2.xml', {}, function(err, faces) {
             var params;
             if (err) throw err;
-            counter++;
-
                 for (var i = 0; i < faces.length; i++) {
+                    counter++;
                     console.log("got face ", counter)
                     params = {
                         CollectionId: 'employees',
@@ -42,7 +42,7 @@ setInterval(function() {
                             Bytes: im.toBuffer()
                         },
                         FaceMatchThreshold: 70,
-                        MaxFaces: 2
+                        MaxFaces: 1
                     };
                     face = faces[i];
 
@@ -66,8 +66,6 @@ setInterval(function() {
                         })
                         .then(data => console.log(data))
                         .catch(err => console.log(err))
-                    //im.rectangle([face.x, face.y], [face.width, face.height], rectColor, rectThickness);
-                    
                 }
             });
         });
